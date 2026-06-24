afa9b9e4fb475354e1fc49f79bf892d51139e6be
MyCodeNotWorking <126179690+MyCodeNotWorking@users.noreply.github.com>
2026-06-24 20:25:23 +0200

    Finish MyGame example (shows how to use engine with plugins); Remove RapierPhysics plugin because the code Claude wrote sucked so the plugin in its current state is useless;

--- examples/MyGame/main.js
+++ examples/MyGame/main.js
@@ -1,25 +1,94 @@
-import { Engine, World } from "../../engine/src/index.js";
-import { createPixiRenderer, createSceneManager, createRapierPhysics, createInput } from '../../plugins/index.js';
+import { Engine, World as EngineWorld, System } from "../../engine/src/index.js";
+import { createPixiRenderer, createSceneManager, createInput, createRapierPixi } from '../../plugins/index.js';
 import { Container, Graphics } from "pixi.js";
+import { World as PhysicsWorld, RigidBodyDesc, ColliderDesc } from "@dimforge/rapier2d";
 import { Viewport } from "pixi-viewport";
 
-const world = new World();
+const world = new EngineWorld();
 const engine = new Engine({ world, fixedDelta: 1/60 });
 
-const pixiPlugin 	= createPixiRenderer();
-const scenePlugin 	= createSceneManager();
-const physicsPlugin = createRapierPhysics();
-const inputPlugin 	= createInput();
+const pixiPlugin 	   = createPixiRenderer();
+const scenePlugin 	   = createSceneManager();
+const inputPlugin 	   = createInput();
+const rapierPixiPlugin = createRapierPixi();
 
 await pixiPlugin.init();
 
-world.use(pixiPlugin);
-world.use(scenePlugin);
-world.use(physicsPlugin);
-world.use(inputPlugin);
+[
+    pixiPlugin,
+    scenePlugin,
+    inputPlugin,
+    rapierPixiPlugin
+].forEach(plugin => world.use(plugin));
+
+const physicsWorld = new PhysicsWorld({ x: 0, y: -50 })
+
+world.defineComponent("RapierPixi", {
+	collider: null,
+	graphics: null,
+});
+
+function syncRapierPixi(c, g) {
+    const pos = c.translation();
+    const rot = c.rotation();
+    g.position.set(pos.x, -pos.y);
+    g.rotation = rot;
+    switch(c.shapeType()) {
+        case 0:
+            g.scale.set(c.radius(), c.radius());
+            break;
+        case 1:
+            g.scale.set(c.halfExtents().x, c.halfExtents().y);
+            break;
+        case 2:
+            g.scale.set(c.radius(), c.halfHeight() + c.radius());
+            break;                
+    }
+}
+
+class RapierPhysicsStepSystem extends System {
+  	constructor() {
+    	super({ phase: "fixedUpdate", order: 0 });
+  	}
+
+  	fixedUpdate(world, dt) {
+    	physicsWorld.step();
+  	}
+}
+
+class RapierPixiSyncSystem extends System {
+  	constructor() {
+    	super({ phase: "lateUpdate", order: 0 });
+  	}
+
+  	lateUpdate(world, dt) {
+    	for (const entity of world.query({ all: ["RapierPixi"] })) {
+      		const sync = world.getComponent(entity, "RapierPixi");
+      		if (!sync?.collider || !sync?.graphics) continue;
+      		syncRapierPixi(sync.collider, sync.graphics);
+    	}
+  	}
+}
+
+world.registerSystem(new RapierPhysicsStepSystem());
+world.registerSystem(new RapierPixiSyncSystem());
+
+function makeRigidBodyObject({ world, physicsWorld, vp, rbd, cd, gfx }) {
+	const e = world.createEntity();
+	const r = physicsWorld.createRigidBody(rbd);
+	const c = physicsWorld.createCollider(cd, r);
+	vp.addChild(gfx);
+	world.addComponent(e, "RapierPixi", { collider: c, graphics: gfx });
+	return {
+		e,
+		r,
+		c,
+		gfx
+	}
+}
 
 function createGameScene({ app }) {
-	let viewport;
+	let viewport;		
 	return {
 		enter(world) {
 			viewport = new Viewport({
@@ -30,9 +99,20 @@ function createGameScene({ app }) {
 			  	events: app.renderer.events, // the interaction module is important for wheel to work properly when renderer.view is placed or scaled
 			});
 			viewport.drag().pinch().wheel().decelerate();
-			pixiPlugin.addDebugCoordinateSystem(viewport, 100);			
-			const rect = new Graphics().rect(100, 0, 100, 100).fill('red');
-			viewport.addChild(rect);
+			viewport.setZoom(50);
+			pixiPlugin.addDebugCoordinateSystem(viewport, 100);
+			const box = makeRigidBodyObject({
+				world: world, physicsWorld: physicsWorld, vp: viewport, 
+				rbd: RigidBodyDesc.dynamic().setTranslation(0, 0),
+				cd: ColliderDesc.cuboid(5, 5),
+				gfx: new Graphics().rect(-1, -1, 2, 2).fill("red")
+			});
+			const ground = makeRigidBodyObject({
+				world: world, physicsWorld: physicsWorld, vp: viewport, 
+				rbd: RigidBodyDesc.fixed().setTranslation(0, -20),
+				cd: ColliderDesc.cuboid(20, 2),
+				gfx: new Graphics().rect(-1, -1, 2, 2).fill("green")
+			});
 			app.stage.addChild(viewport);
 		}
 	}
--- plugins/index.js
+++ plugins/index.js
@@ -2,3 +2,4 @@ export * from "./scenes/index.js";
 export * from "./renderers/index.js";
 export * from "./physics/index.js";
 export * from "./input/index.js";
+export * from "./rapierPixi/index.js";
